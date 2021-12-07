728ae6c41a4af591db4ad9f5539cc003bde0d244
Massurio <massurio@gmail.com>
2021-12-07 18:51:30 +0000

    soundChec31

--- main.js
+++ main.js
@@ -2,51 +2,83 @@ window.addEventListener('keydown', (event) => {
   if (event.keyCode == 65) {
     var clapA = new Audio('./sounds/clap.wav');
     clapA.play();
-    document.getElementById('clapA').style.transform = 'rotate(360deg)';
-    document.getElementById('clapA').style.transitionDuration = '3s';
+  } else if (event.keyCode == 83) {
+    var hiHatS = new Audio('./sounds/hiHat.wav');
+    hiHatS.play();
   } else if (event.keyCode == 68) {
     var kickD = new Audio('./sounds/kick.wav');
     kickD.play();
-    document.getElementById('kickD').style.transform = 'rotate(360deg)';
-    document.getElementById('kickD').style.transitionDuration = '2s';
+  } else if (event.keyCode == 70) {
+    var openHatF = new Audio('./sounds/openHat.wav');
+    openHatF.play();
   } else if (event.keyCode == 71) {
     var boomG = new Audio('./sounds/boom.wav');
     boomG.play();
-    document.getElementById('boomG').style.transform = 'rotate(360deg)';
-    document.getElementById('boomG').style.transitionDuration = '5s';
   } else if (event.keyCode == 72) {
     var rideH = new Audio('./sounds/ride.wav');
     rideH.play();
-    document.getElementById('rideH').style.transform = 'rotate(360deg)';
-    document.getElementById('rideH').style.transitionDuration = '6s';
   } else if (event.keyCode == 74) {
     var snareJ = new Audio('./sounds/snare.wav');
     snareJ.play();
-    document.getElementById('snareJ').style.transform = 'rotate(360deg)';
-    document.getElementById('snareJ').style.transitionDuration = '3s';
   } else if (event.keyCode == 75) {
     var tomK = new Audio('./sounds/tom.wav');
     tomK.play();
-    document.getElementById('tomK').style.transform = 'rotate(360deg)';
-    document.getElementById('tomK').style.transitionDuration = '4s';
   } else if (event.keyCode == 76) {
     var tinkL = new Audio('./sounds/tink.wav');
     tinkL.play();
-    document.getElementById('tinkL').style.transform = 'rotate(360deg)';
-    document.getElementById('tinkL').style.transitionDuration = '2s';
   }
 });
-window.addEventListener('keydown', (event) => {
-  if (event.keyCode == 83) {
-    var hiHatS = new Audio('./sounds/hiHat.wav');
-    hiHatS.play();
-    document.getElementById('hiHatS').style.transform = 'rotate(360deg)';
-    document.getElementById('hiHatS').style.transitionDuration = '7s';
-  }
-  if (event.keyCode == 70) {
-    var openHatF = new Audio('./sounds/openHat.wav');
-    openHatF.play();
-    document.getElementById('openHatF').style.transform = 'rotate(360deg)';
-    document.getElementById('openHatF').style.transitionDuration = '4s';
-  }
+const clapA = document.getElementById('clapA');
+clapA.addEventListener('click', () => {
+  console.log('Hello');
+  var clapA = new Audio('./sounds/clap.wav');
+  clapA.play();
+});
+const hiHatS = document.getElementById('hiHatS');
+hiHatS.addEventListener('click', () => {
+  console.log('Hello');
+  var hiHatS = new Audio('./sounds/hiHat.wav');
+  hiHatS.play();
+});
+const kickD = document.getElementById('kickD');
+kickD.addEventListener('click', () => {
+  console.log('Hello');
+  var kickD = new Audio('./sounds/kick.wav');
+  kickD.play();
+});
+const openHatF = document.getElementById('openHatF');
+openHatF.addEventListener('click', () => {
+  console.log('Hello');
+  var openHatF = new Audio('./sounds/openHat.wav');
+  openHatF.play();
+});
+const boomG = document.getElementById('boomG');
+boomG.addEventListener('click', () => {
+  console.log('Hello');
+  var boomG = new Audio('./sounds/boom.wav');
+  boomG.play();
+});
+const rideH = document.getElementById('rideH');
+rideH.addEventListener('click', () => {
+  console.log('Hello');
+  var rideH = new Audio('./sounds/ride.wav');
+  rideH.play();
+});
+const snareJ = document.getElementById('snareJ');
+snareJ.addEventListener('click', () => {
+  console.log('Hello');
+  var snareJ = new Audio('./sounds/snare.wav');
+  snareJ.play();
+});
+const tomK = document.getElementById('tomK');
+tomK.addEventListener('click', () => {
+  console.log('Hello');
+  var tomK = new Audio('./sounds/tom.wav');
+  tomK.play();
+});
+const tinkL = document.getElementById('tinkL');
+tinkL.addEventListener('click', () => {
+  console.log('Hello');
+  var tinkL = new Audio('./sounds/tink.wav');
+  tinkL.play();
 });
